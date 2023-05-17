4e1973aca103403211a9b92cf190f30e862a46e5
SunDoge <384813529@qq.com>
2023-05-17 17:38:04 +0800

    add from_buffer for opencv

--- firecore/opencv.py
+++ firecore/opencv.py
@@ -9,14 +9,23 @@ except ImportError:
     logger.exception("you should install opencv")
 
 
-def cv2_loader(filename: str, flags: int = cv2.IMREAD_COLOR) -> np.ndarray:
-    """
-    Return RGB image as np.ndarray
-    """
-    img = cv2.imread(filename, flags=flags)
+def read_rgb(filename: str) -> np.ndarray:
+    img = cv2.imread(filename, flags=cv2.IMREAD_COLOR)
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 
 def cv2_worker_init_fn(worker_id: int):
     cv2.setNumThreads(0)
     logger.info("cv2 get num threads: %d", cv2.getNumThreads())
+
+
+def decode_from_buffer(buf: bytes, flags: int = cv2.IMREAD_COLOR) -> np.ndarray:
+    img_buf = np.frombuffer(buf, dtype=np.uint8)
+    img = cv2.imdecode(img_buf, flags)
+    return img
+
+
+def decode_rgb(buf: bytes) -> np.ndarray:
+    img = decode_from_buffer(buf)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
