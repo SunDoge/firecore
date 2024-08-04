553d27406b72f372173544f26cc6d067b1080dc1
SunDoge <triplez0@outlook.com>
2024-08-04 23:07:29 +0800

    update

--- src/firecore/main_utils.py
+++ src/firecore/main_utils.py
@@ -6,7 +6,7 @@ import importlib.util
 T = TypeVar("T")
 
 
-def _main_fn(func: T) -> T:
+def _main_fn(func: Callable):
     # 拿到调用这个函数的 FrameInfo
     caller = inspect.stack()[2]
     # import ipdb; ipdb.set_trace()
@@ -16,14 +16,16 @@ def _main_fn(func: T) -> T:
     if name == "__main__":
         file_path = caller.frame.f_globals["__file__"]
         spec = importlib.util.spec_from_file_location("__fake_main", file_path)
+        assert spec is not None
         module = importlib.util.module_from_spec(spec)
+        assert spec.loader is not None
         spec.loader.exec_module(module)
         getattr(module, func.__name__)()
     else:
         return func
 
 
-def main_fn(func: Optional[T]) -> Union[Callable[[T], T], T]:
+def main_fn(func: Optional[Callable]):
     if func:
         return _main_fn(func)
     else:
